18aedd0064c1a400e4d84ca8b3d0877b86f0a41d
EvgYastrebov <118362486+EvgYastrebov@users.noreply.github.com>
2024-05-02 20:38:16 +0500

    Update and rename config.py to Config.py

---
+++ Config.py
@@ -0,0 +1,9 @@
+db_name = "localhost"
+db_user = "postgres"
+db_password = "postgres"
+db_host = "localhost"
+
+
+symbol_mode = 0
+key_mode = 1
+answer_under_question_mode = 2
--- config.py
+++
@@ -1,9 +0,0 @@
-db_name = "postgres"
-db_user = "postgres"
-db_password = "cgb8859p"
-db_host = "localhost"
-
-
-symbol_mode = 0
-key_mode = 1
-answer_under_question_mode = 2
